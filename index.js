d1036444560669d294309be059487ec974a6edb8
Spencer Holman <spencer.a.holman@gmail.com>
2017-07-17 14:01:14 -0600

    throw err.stack as well

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ module.exports = function(param, callback) {
         param = JSON.parse(param)
         return param;
     } catch(err) {
-        throw("JSON.parse error: " + err.message + ", parameter=" + param)
+        throw("JSON.parse error: " + err.message + ", parameter=" + param + err.stack)
     }
 }
